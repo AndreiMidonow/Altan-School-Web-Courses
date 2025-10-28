3f55dbc6d2693965251bdbebfd89de361b007a7a
AndreiMidonow <Midonovmidonov@yandex.ru>
2025-10-28 22:42:01 +0100

    No scroll for "open button"

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
+const scrollController = {
+  disabledScroll() {
+    document.body.style.cssText = `
+      overflow: hidden;
+      position: fixed;
+      top: 0;
+      left: 0;
+      height: 100vh;
+      width: 100vw;
+      padding-right: ${window.innerWidth - document.body.offsetWidth}px
+    `;
+  },
+  enabledScroll() {
+    document.body.style.cssText = ''; 
+  },
+}
+
+
 const nav = document.querySelector('#nav');
 const navbtn = document.querySelector('#nav-btn');
 const navbtnimg = document.querySelector('#nav-btn-img');
 navbtn.onclick = () => {
     if (nav.classList.toggle('open')){
         navbtnimg.src = "./img/NAV CLOSE.svg"
+        scrollController.disabledScroll();
     } else {
         navbtnimg.src = "./img/NAV OPEN.svg"
+        scrollController.enabledScroll(); 
     }
 }
